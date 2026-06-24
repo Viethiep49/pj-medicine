aab2fb0ed088fdfb1e57748996bfc02d87a2229b
Viethiep49 <truongviethiep49@gmail.com>
2026-06-24 10:01:24 +0700

    chore(main): chỉ giữ README, toàn bộ code chuyển sang nhánh dev

--- backend/app/schemas/__init__.py
+++
@@ -1,9 +0,0 @@
-from .patient import PatientBase, PatientCreate, PatientResponse
-from .record import MedicalRecordBase, MedicalRecordCreate, MedicalRecordResponse
-from .prediction import PredictionRequest, PredictionResultItem, PredictionResponse, XAITokenSchema, ExplainResponse
-
-__all__ = [
-    "PatientBase", "PatientCreate", "PatientResponse",
-    "MedicalRecordBase", "MedicalRecordCreate", "MedicalRecordResponse",
-    "PredictionRequest", "PredictionResultItem", "PredictionResponse", "XAITokenSchema", "ExplainResponse"
-]
